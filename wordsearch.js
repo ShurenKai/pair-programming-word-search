25b3fe5f2530542f245d16785deb6561686f4d94
Ren <cassandra.chen@uoit.net>
2021-12-02 19:38:51 +0000

    word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,18 @@
+const transpose = require("../../../matrix_transposition")
+
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+  const horizontalJoin = letters.map(ls => ls.join(''))
+  const vertical = transpose(letters).map(ls => ls.join(''))
+  let isWord = false
+  for (l of horizontalJoin) {
+      if (l.includes(word))
+      isWord = true
+  }
+  for (l of vertical) {
+      if (l.includes(word))
+      isWord = true
+  }
+  return isWord
+};
 
 module.exports = wordSearch
